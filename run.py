713a4bdc5c4b73c48464e991cd7a3e43419d3ab1
Ubuntu <ubuntu@ip-172-31-19-80.eu-west-1.compute.internal>
2019-07-20 18:06:09 +0000

    completed chat app with styling and added Procfile

--- run.py
+++ run.py
@@ -1,31 +1,45 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "arandomstring")
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
     """ Add messages to stack """
-    messages.append("{}: {}".format(username, message))
-    
-def format_messages_for_display():
-    """ Format the messages for nice display """
-    return "<br>".join(messages)
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({
+        "timestamp" : now,
+        "from" : username,
+        "message" : message
+    })
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
     """ Main page with user directions """
-    return "<p>To send a message use /<i>username</i>/<i>message</i></p>"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
     
-@app.route('/<username>')
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+        
+    return render_template('index.html')
+    
+@app.route('/chat/<username>', methods=["GET","POST"])
 def user(username):
     """ User page with messages """
-    return "<h1>Hi {}, your messages:</h1>{}".format(username, format_messages_for_display())
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+    return render_template('chat.html', username = username, chat_messages = messages)
     
-@app.route('/<username>/<message>')
+@app.route('/chat/<username>/<message>')
 def send_message(username, message):
     """ Take new message and add to chat """
-    add_messages(username, message)
-    return redirect("/" + username)
+    add_message(username, message)
+    return redirect(url_for("user", username=session["username"]))
     
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+app.run(host=os.getenv('IP', "0.0.0.0"), port=int(os.getenv('PORT', "5000")), debug=False)
